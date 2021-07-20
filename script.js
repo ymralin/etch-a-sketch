4e01fd2e15c8868c40940521ff18fbdc764e60a0
Adam <adam_sob@o2.pl>
2021-07-21 00:26:59 +0200

    added board resizing functionality. set board size to 4x4 to 64x64. set board ratio to 2:3 to 3:2. default size 16x16

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const sketchBoard = document.querySelector('.sketchBoard');
-for (i=0; i<256; i++) {
-    const div = document.createElement('div');
-    div.className = 'divCell';
-    sketchBoard.appendChild(div);
-        div.addEventListener('mouseover', paintingAction);
-}
+// const sketchBoard = document.querySelector('.sketchBoard');
+// for (i=0; i<256; i++) {
+//     const div = document.createElement('div');
+//     div.className = 'divCell';
+//     sketchBoard.appendChild(div);
+//         div.addEventListener('mouseover', paintingAction);
+// }
 
 function paintingAction(e){
     console.log(this)
@@ -26,8 +26,8 @@ function erase(e) {
 
 let painting=true;
 
+const settingsContainer = document.querySelector('.setting')
 const buttonContainer = document.querySelector('.buttonContainer');
-
 
 for (i=0; i<2; i++){
     const btn = document.createElement('div');
@@ -38,9 +38,11 @@ for (i=0; i<2; i++){
     btn.addEventListener('transitionend', removeTransition);
 };
 
+
+
 function clicked(e) {
     this.classList.add('clicked');
-    console.log(this);
+    // console.log(this);
 }
 
 function removeTransition(e){
@@ -69,3 +71,63 @@ function resetSketch() {
     const cells=document.querySelectorAll('.divCell');
     cells.forEach(cell => cell.classList.remove('painted'))
 };
+
+const resizeBtn = document.querySelector('#resizeBtn')
+resizeBtn.addEventListener('click', clicked);
+resizeBtn.addEventListener('transitionend', removeTransition);
+resizeBtn.textContent = 'Resize';
+resizeBtn.addEventListener('click', resizeBoard);
+
+
+
+function resizeBoard() {
+    const colNumInput = document.querySelector('#colsNum');
+    const rowNumInput = document.querySelector('#rowsNum');
+    const colNum = Number(colNumInput.value);
+    const rowNum = Number(rowNumInput.value);
+    if ((!isNaN(colNum)) && (!isNaN(rowNum))) {
+        if ((4<=colNum && colNum<=64) && (4<=rowNum && rowNum<=64)){
+            if (colNum<=rowNum*1.5 && rowNum<=colNum*1.5) {
+                drawBoard(colNum, rowNum)
+                colNumInput.value = "";
+                rowNumInput.value = "";
+                painting=true;
+                btn1.classList.remove('toggled');
+            
+            }
+        }
+    }
+
+}
+
+function drawBoard(cols, rows) {
+    clearBoard();
+    
+    const sketchBoard = document.querySelector('.sketchBoard');
+    sketchBoard.style.width = '640px'
+    sketchBoard.style.height = rows/cols * 640 + 'px'
+    for (i=0; i<cols; i++) {
+        for (j=0; j<rows; j++) {
+            const div = document.createElement('div');
+            div.className = 'divCell';
+            div.id = 'divCell' + i + "-" + j;
+            sketchBoard.appendChild(div);
+            div.addEventListener('mouseover', paintingAction);
+            div.style.width = sketchBoard.style.width / cols
+            div.style.height = sketchBoard.style.height / rows
+        }
+    }
+    let cT
+    cT = ""
+    for (i=0; i<cols; i++) {
+        cT = cT + "1fr "
+    }
+    sketchBoard.style.gridTemplateColumns = cT
+}
+
+function clearBoard() {
+    document.querySelectorAll('.divCell').forEach(div => document.querySelector('.sketchBoard').removeChild(div))
+}
+
+drawBoard(16,16);
+
